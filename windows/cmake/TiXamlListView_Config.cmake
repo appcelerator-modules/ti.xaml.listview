ed7b124e639648f0e6ca52c97dd4847567e08f3f
Kota Iguchi <developer@infosia.co.jp>
2017-07-28 14:50:48 +0900

    [TIMOB-24761] Lightweight ListView for Windows

---
+++ windows/cmake/TiXamlListView_Config.cmake
@@ -0,0 +1,11 @@
+# Titanium Windows Native Module - ti.xaml.listview
+#
+# Copyright (c) 2017 by Appcelerator, Inc. All Rights Reserved.
+# Licensed under the terms of the Apache Public License.
+# Please see the LICENSE included with this distribution for details.
+
+include(CMakeFindDependencyMacro)
+find_dependency(HAL)
+find_dependency(TitaniumKit)
+
+include("${CMAKE_BINARY_DIR}/TitaniumWindows_TiXamlListView_Targets.cmake")
